b4b5a3796043deaf212114436ccb9d6068403028
Ahmed Azab <aazab22@mail.ru>
2024-11-30 20:12:28 +0500

    Initial project with new Assignments

---
+++ Projects/project1.py
@@ -0,0 +1,10 @@
+print("Welcome to the YouTube Channel Name Generator")
+
+# Read user input for nickname
+user_name = input("What is your nickname?\n")
+
+# Read user input for channel topic
+channel_name = input("What is your channel about?\n")
+
+# Generate and print the suggested channel name
+print("You could name your channel (", channel_name, "with", user_name, ")")
